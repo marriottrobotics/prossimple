213f6762d7fd05fd47d630c1c6137ed7feaf9212
Zenith08 <kaidenr@shaw.ca>
2018-10-04 13:52:34 -0700

    Made into a library I hope.

---
+++ template.mk
@@ -0,0 +1,20 @@
+LIBNAME=libemssimple
+VERSION=0.0.1
+
+# extra files (like header files)
+TEMPLATEFILES = include/GameState.h include/Movement.h include/Pid_Core.h include/Sensors.h
+# basename of the source files that should be archived
+TEMPLATEOBJS = GameState Movement Pid_Core Sensors
+
+TEMPLATE=$(ROOT)/$(LIBNAME)-template
+
+.DEFAULT_GOAL: all
+
+library: clean $(BINDIR) $(SUBDIRS) $(ASMOBJ) $(COBJ) $(CPPOBJ)
+		$(MCUPREFIX)ar rvs $(BINDIR)/$(LIBNAME).a $(foreach f,$(TEMPLATEOBJS),$(BINDIR)/$(f).o)
+		mkdir -p $(TEMPLATE) $(TEMPLATE)/firmware $(addprefix $(TEMPLATE)/, $(dir $(TEMPLATEFILES)))
+		cp $(BINDIR)/$(LIBNAME).a $(TEMPLATE)/firmware/$(LIBNAME).a
+		$(foreach f,$(TEMPLATEFILES),cp $(f) $(TEMPLATE)/$(f);)
+		pros conduct create-template $(LIBNAME) $(VERSION) $(TEMPLATE) --ignore template.pros --upgrade-files firmware/$(LIBNAME).a $(foreach f,$(TEMPLATEFILES),--upgrade-files $(f))
+		@echo Need to zip $(TEMPLATE) without the base directory
+		# cd $(TEMPLATE) && zip -r ../$(basename $(TEMPLATE)) *
